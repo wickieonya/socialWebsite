b2cdac1f4214438011f326589ece1bbd3469eddc
wickieonya <wickieonya@gmail.com>
2020-09-21 08:41:43 +0300

    updated with signals

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -7,8 +7,10 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 from django.views.decorators.http import require_POST
 from common.decorators import ajax_required
+from actions.utils import create_action
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile, Contact
+from actions.models import Action
 
 
 def register(request):
@@ -19,6 +21,7 @@ def register(request):
             new_user.set_password(user_form.cleaned_data["password"])
             new_user.save()
             Profile.objects.create(user=new_user)
+            create_action(new_user, "has created an account")
             return render(request, "account/register_done.html", {"new_user": new_user})
     else:
         user_form = UserRegistrationForm()
@@ -48,7 +51,14 @@ def user_login(request):
 
 @login_required
 def dashboard(request):
-    return render(request, "account/dashboard.html", {"section": "dashboard"})
+    # Display all actions by default
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list("id", flat=True)
+    if following_ids:
+        # If user is following others, retrieve only their actions
+        actions = actions.filter(user_id__in=following_ids)
+        actions = actions.select_related('user', 'user__profile').prefetch_related('target')[:10]
+    return render(request, "account/dashboard.html", {"section": "dashboard", "actions": actions})
 
 
 @login_required
@@ -102,6 +112,7 @@ def user_follow(request):
             user = User.objects.get(id=user_id)
             if action == "follow":
                 Contact.objects.get_or_create(user_from=request.user, user_to=user)
+                create_action(request.user, "is following", user)
             else:
                 Contact.objects.filter(user_from=request.user, user_to=user).delete()
             return JsonResponse({"status": "ok"})
--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "social_django",
     "django_extensions",
     "easy_thumbnails",
+    "actions.apps.ActionsConfig",
 ]
 
 MIDDLEWARE = [
